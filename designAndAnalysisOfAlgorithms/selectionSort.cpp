71361ce25a1b0f2c99edc70ae3c5952647bc08bd
d33panpaudel07 <dodeepan7@gmail.com>
2025-01-21 17:46:50 +0545

    did selectionSort.cpp

---
+++ designAndAnalysisOfAlgorithms/selectionSort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[10] = {2, 5, 3, 1, 8, 93, 7, 55, 11, 0};
+    int curr_min;
+    int temp;
+
+    for (int i = 0; i < 10; i++)
+    {
+        curr_min = i;
+        for (int j = i + 1; j < 10; j++)
+        {
+            if (arr[j] < arr[curr_min])
+            {
+                curr_min = j;
+            }
+        }
+        temp = arr[curr_min];
+        arr[curr_min] = arr[i];
+        arr[i] = temp;
+    }
+
+    for (int k = 0; k < 10; k++)
+    {
+        cout << arr[k] << ", ";
+    }
+}
