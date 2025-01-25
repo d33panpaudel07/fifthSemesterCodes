9511078a9c8908a269aef186062ac38c03b9212c
d33panpaudel07 <141331201+d33panpaudel07@users.noreply.github.com>
2025-01-25 15:36:54 +0545

    Update insertionSort.cpp
    added missing counter

--- designAndAnalysisOfAlgorithms/insertionSort.cpp
+++ designAndAnalysisOfAlgorithms/insertionSort.cpp
@@ -15,30 +15,27 @@ void insertionSort()
 
     for (int i = 1; i < 10; i++)
     {
-        // cout << "ITERATION " << i << endl;
-
         int current = arr[i];
         int j = i - 1;
 
-        // Move elements of arr[0..i-1], that are greater than current, to one position ahead
         while (j >= 0 && arr[j] > current)
         {
-            exchange(arr[j + 1],arr[j]);
+            exchange(arr[j + 1], arr[j]);
             j--;
+            stepsCounter++;
         }
         arr[j + 1] = current;
-
-        // Display the sorting step by step
-        cout << "Iteration " << i << " completed." << endl;
+        stepsCounter++;
     }
 
-    // Print the sorted array
     cout << "Sorted Array: ";
     for (int i = 0; i < 10; i++)
     {
         cout << arr[i] << ", ";
     }
     cout << endl;
+
+    cout << "Total steps: " << stepsCounter << endl;
 }
 
 int main()
