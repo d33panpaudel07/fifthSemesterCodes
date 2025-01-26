5b669673047cfd9ccc88901c3ea07df2678155c4
d33panpaudel07 <141331201+d33panpaudel07@users.noreply.github.com>
2025-01-26 20:54:51 +0545

    Update BST.cpp
    minimized the bstCode and added a stepsCounter

--- designAndAnalysisOfAlgorithms/BST.cpp
+++ designAndAnalysisOfAlgorithms/BST.cpp
@@ -1,217 +1,45 @@
 #include <iostream>
 using namespace std;
 
-class Node
-{
+class Node {
 public:
     int data;
-    Node *parent = NULL;
-    Node *childL = NULL;
-    Node *childR = NULL;
+    Node* left;
+    Node* right;
 
-public:
-    Node()
-    {
-        this->parent = NULL;
-        this->childL = NULL;
-        this->childR = NULL;
-        this->data = 0;
-    }
+    Node(int value) : data(value), left(NULL), right(NULL) {}
 };
 
-class BinarySearchTree
-{
-public:
-    Node *root = NULL;
-    Node *curr = NULL;
-    int val;
-    int counter = 0; // Global counter to track the number of steps
+Node* insert(Node* root, int value) {
+    if (!root) return new Node(value);
+    if (value < root->data) root->left = insert(root->left, value);
+    else root->right = insert(root->right, value);
+    return root;
+}
 
-public:
-    BinarySearchTree()
-    {
-        this->root = NULL;
-    }
+bool searchBST(Node* root, int value, int& steps) {
+    steps++;
+    if (!root) return false;
+    if (value == root->data) return true;
+    if (value < root->data) return searchBST(root->left, value, steps);
+    return searchBST(root->right, value, steps);
+}
 
-    int checkChildOfNode(Node *temp) // to check the status of the node
-    {
-        counter++;                                            // Increment counter for each method call
-        if ((temp->childL == NULL) && (temp->childR == NULL)) // assuming the node is leaf node
-        {
-            return 0;
-        }
-        else if ((temp->childL != NULL) && (temp->childR != NULL)) // assuming the node has both left and right child
-        {
-            return 2;
-        }
-        else // assuming the node has at least one child
-        {
-            return 1;
-        }
-    }
+int main() {
+    Node* root = NULL;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 70);
+    insert(root, 20);
+    insert(root, 40);
 
-    Node *insertNodeFinder(Node *temp) // root goes to temp
-    {
-        counter++; // Increment counter for each method call
-        if (temp == NULL)
-        {
-            return NULL;
-        }
-        else if (val <= temp->data)
-        {
-            if (temp->childL == NULL)
-            {
-                cout << "ON THE LEFT, Below " << temp->data << endl;
-                return temp;
-            }
-            temp = insertNodeFinder(temp->childL);
-        }
-        else
-        {
-            if (temp->childR == NULL)
-            {
-                cout << "ON THE RIGHT, Below " << temp->data << endl;
-                return temp;
-            }
-            temp = insertNodeFinder(temp->childR);
-        }
-        return temp;
-    }
+    int searchValue;
+    cout << "Enter value to search: ";
+    cin >> searchValue;
 
-    Node *deletePlaceFinder(Node *temp) // root goes to temp
-    {
-        counter++; // Increment counter for each method call
-        if (temp == NULL)
-        {
-            return NULL;
-        }
-        else if (temp->data == val)
-        {
-            return temp;
-        }
-        else
-        {
-            Node *result;
-            result = deletePlaceFinder(temp->childL);
-            if (result != NULL)
-            {
-                return result;
-            }
-            result = deletePlaceFinder(temp->childR);
-            return result;
-        }
-    }
+    int stepsCounter = 0;
+    if (searchBST(root, searchValue, stepsCounter)) cout << "Found in " << stepsCounter << " steps!" << endl;
+    else cout << "Not Found after " << stepsCounter << " steps!" << endl;
 
-    Node *leftReplacementNodeFinderForDel(Node *temp) // to find the greatest child to replace from left
-    {
-        counter++; // Increment counter for each method call
-        if (temp->childR == NULL)
-        {
-            return temp;
-        }
-        else
-        {
-            temp = leftReplacementNodeFinderForDel(temp->childR);
-            return temp;
-        }
-    }
-
-    void insert(int x)
-    {
-        counter++; // Increment counter for each method call
-        val = x;
-        curr = insertNodeFinder(root); // returns temp that points to the desired location
-
-        Node *newNode = new Node;
-
-        if (curr == NULL)
-        {
-            newNode->data = val;
-            root = newNode;
-            return;
-        }
-        else if (val <= curr->data)
-        {
-            curr->childL = newNode;
-        }
-        else
-        {
-            curr->childR = newNode;
-        }
-        newNode->parent = curr;
-        newNode->data = val;
-        cout << "Val is : " << newNode->data << endl;
-    }
-
-    void deleteFromTree(int val)
-    {
-        counter++; // Increment counter for each method call
-        this->val = val;
-        curr = deletePlaceFinder(root);
-
-        if (curr == NULL)
-        {
-            cout << "Could not find the given element." << endl;
-        }
-        else // assuming we replace deleted element with left child
-        {
-            Node *temp = curr->childL; // selecting the left child of the node to delete
-            temp = leftReplacementNodeFinderForDel(temp);
-
-            curr->data = temp->data;
-            temp->parent->childR = NULL;
-            delete temp;
-        }
-    }
-
-    void preTraverse(Node *temp)
-    {
-        counter++; // Increment counter for each method call
-        if (temp == NULL)
-        {
-            cout << "Can't traverse the tree as it is empty." << endl;
-            return;
-        }
-        else
-        {
-            cout << "Current Node: " << temp->data << endl;
-            if (temp->childL != NULL)
-            {
-                preTraverse(temp->childL);
-            }
-            if (temp->childR != NULL)
-            {
-                preTraverse(temp->childR);
-            }
-            return;
-        }
-    }
-
-    void mainProcess()
-    {
-        insert(20);
-        insert(5);
-        insert(17);
-        insert(8);
-        insert(50);
-        insert(23);
-        insert(62);
-        insert(25);
-        insert(33);
-        insert(9);
-        insert(81);
-
-        preTraverse(root);
-
-        deleteFromTree(50);
-        preTraverse(root);
-
-        cout << "Total steps taken: " << counter << endl;
-    }
-};
-
-int main()
-{
-    BinarySearchTree b1;
-    b1.mainProcess();
+    return 0;
 }
