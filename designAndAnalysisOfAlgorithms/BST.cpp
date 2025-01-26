2bf62ac452734b56a4512b4eb7194a1002fe4dcf
d33panpaudel07 <dodeepan7@gmail.com>
2025-01-26 20:56:56 +0545

    updated bst

--- designAndAnalysisOfAlgorithms/BST.cpp
+++ designAndAnalysisOfAlgorithms/BST.cpp
@@ -1,32 +1,42 @@
 #include <iostream>
 using namespace std;
 
-class Node {
+class Node
+{
 public:
     int data;
-    Node* left;
-    Node* right;
+    Node *left;
+    Node *right;
 
     Node(int value) : data(value), left(NULL), right(NULL) {}
 };
 
-Node* insert(Node* root, int value) {
-    if (!root) return new Node(value);
-    if (value < root->data) root->left = insert(root->left, value);
-    else root->right = insert(root->right, value);
+Node *insert(Node *root, int value)
+{
+    if (!root)
+        return new Node(value);
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else
+        root->right = insert(root->right, value);
     return root;
 }
 
-bool searchBST(Node* root, int value, int& steps) {
+bool searchBST(Node *root, int value, int &steps)
+{
     steps++;
-    if (!root) return false;
-    if (value == root->data) return true;
-    if (value < root->data) return searchBST(root->left, value, steps);
+    if (!root)
+        return false;
+    if (value == root->data)
+        return true;
+    if (value < root->data)
+        return searchBST(root->left, value, steps);
     return searchBST(root->right, value, steps);
 }
 
-int main() {
-    Node* root = NULL;
+int main()
+{
+    Node *root = NULL;
     root = insert(root, 50);
     insert(root, 30);
     insert(root, 70);
@@ -38,8 +48,10 @@ int main() {
     cin >> searchValue;
 
     int stepsCounter = 0;
-    if (searchBST(root, searchValue, stepsCounter)) cout << "Found in " << stepsCounter << " steps!" << endl;
-    else cout << "Not Found after " << stepsCounter << " steps!" << endl;
+    if (searchBST(root, searchValue, stepsCounter))
+        cout << "Found in " << stepsCounter << " steps!" << endl;
+    else
+        cout << "Not Found after " << stepsCounter << " steps!" << endl;
 
     return 0;
 }
